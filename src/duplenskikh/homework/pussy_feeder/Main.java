25e4ac1f137a10adc6160652ec80a182ca83621a
duplenskikh <ss_1o1@mail.ru>
2021-10-20 22:16:52 +0500

    Pussy feeder

--- src/duplenskikh/homework/pussy_feeder/Main.java
+++ src/duplenskikh/homework/pussy_feeder/Main.java
@@ -18,8 +18,115 @@ package duplenskikh.homework.pussy_feeder;
 
 после ввода программа каждый раз выводит у кого сколько корма и меню
 */
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        pussyFeeder();
+    }
 
+    private static final int FULL_FEEDER_VALUE = 7;
+    private static final int EMPTY_FEEDER_VALUE = 0;
+    private static final int INDEX_CORRECTION = 1;
+
+    private static int[] feeders = new int[7];
+
+    private static void pussyFeeder() {
+        System.out.println(Utils.ENTRY_MESSAGE);
+        printMenu();
+        Scanner scan = new Scanner(System.in);
+        do {
+            switch (scan.nextInt()) {
+                case 1: {
+                    System.out.println(Utils.QUESTION_CAT_MESSAGE);
+                    int feederIndex = scan.nextInt() - INDEX_CORRECTION;
+                    System.out.println(Utils.QUESTION_FOOD_MESSAGE);
+                    int foodValue = scan.nextInt();
+                    feedPussy(feederIndex, foodValue);
+                    printFeedersValues();
+                    break;
+                }
+                case 2: {
+                    System.out.println(Utils.QUESTION_FOOD_MESSAGE);
+                    int foodValue = scan.nextInt();
+                    feedAllPussies(foodValue);
+                    printFeedersValues();
+                    break;
+                }
+                case 3: {
+                    System.out.println(Utils.QUESTION_FOOD_MESSAGE);
+                    int foodValue = scan.nextInt();
+                    feedEvenOrOddPussies(foodValue, true);
+                    printFeedersValues();
+                    break;
+                }
+                case 4: {
+                    System.out.println(Utils.QUESTION_FOOD_MESSAGE);
+                    int foodValue = scan.nextInt();
+                    feedEvenOrOddPussies(foodValue, false);
+                    printFeedersValues();
+                    break;
+                }
+                case 5: {
+                    feedAllPussiesCompletely();
+                    printFeedersValues();
+                    break;
+                }
+                case 666: {
+                    System.out.println(Utils.EXIT_MESSAGE);
+                    return;
+                }
+                default: {
+                    System.out.println(Utils.ERROR_MESSAGE);
+                    break;
+                }
+            }
+        } while (true);
+    }
+
+    private static void feedPussy(int index, int value) {
+        int total = feeders[index] + value;
+        if (total > FULL_FEEDER_VALUE) {
+            feeders[index] = FULL_FEEDER_VALUE;
+            System.out.println("У кота под номером " + (index + INDEX_CORRECTION) + " полная миска(7 пакетиков)!");
+            return;
+        }
+        if (total < 0) {
+            feeders[index] = EMPTY_FEEDER_VALUE;
+            System.out.println("У кота под номером" + (index + INDEX_CORRECTION) + " пустая миска!");
+            return;
+        }
+        feeders[index] = total;
+    }
+
+    private static void feedAllPussies(int value) {
+        for (int i = 0; i < feeders.length; i++) {
+            feedPussy(i, value);
+        }
+    }
+
+    private static void feedAllPussiesCompletely() {
+        for (int i = 0; i < feeders.length; i++) {
+            feeders[i] = FULL_FEEDER_VALUE;
+        }
+    }
+
+    private static void feedEvenOrOddPussies(int value, boolean isEven) {
+        int startPosition = isEven ? 1 : 0;
+        for (int i = startPosition; i < feeders.length; i += 2) {
+            feedPussy(i, value);
+        }
+    }
+
+    private static void printMenu() {
+        System.out.println(Utils.COMMANDS_MESSAGE);
+    }
+
+    private static void printFeedersValues() {
+        for (int i = 0; i < feeders.length; i++) {
+            System.out.println("В миске номер " + (i + INDEX_CORRECTION) + " содержится " + feeders[i] + " пакетика еды");
+        }
+        printMenu();
     }
 }
---
+++ src/duplenskikh/homework/pussy_feeder/Utils.java
@@ -0,0 +1,16 @@
+package duplenskikh.homework.pussy_feeder;
+
+public class Utils {
+    public static final String ENTRY_MESSAGE = "Приветствую Вас в игре 'Накорми киску'!";
+    public static final String COMMANDS_MESSAGE = "Выберите действие: " +
+            "\n 1 - Накормить одного кота" +
+            "\n 2 - Накормить всех котов" +
+            "\n 3 - Накормить четных котов" +
+            "\n 4 - Накормить нечетных котов" +
+            "\n 5 - Накормить всех котов полностью" +
+            "\n 666 - Выйти";
+    public static final String QUESTION_FOOD_MESSAGE = "Сколько еды положить? (Или отнять, указав отрицательное значение)";
+    public static final String QUESTION_CAT_MESSAGE = "Выберите кота (введите номер миски от 1 до 7)";
+    public static final String ERROR_MESSAGE = "Такой команды не существует";
+    public static final String EXIT_MESSAGE = "До скорой встречи!";
+}
